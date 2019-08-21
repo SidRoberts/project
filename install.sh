706d342e9cf6a8eb24a463512cca3716fe8ec67c
Sid Roberts <sid@sidroberts.co.uk>
2019-08-21 15:52:43 +0900

    Moved nginx conf to its own file

--- install.sh
+++ install.sh
@@ -20,47 +20,7 @@ sudo apt-get -y dist-upgrade
 ################################################################################
 # Install nginx
 
-cat <<EOF | sudo tee -a /etc/nginx/sites-enabled/web
-server {
-    listen 80 default_server;
-
-    server_name web.dev;
-
-    charset utf-8;
-
-    root '/app/';
-
-    sendfile off;
-
-    client_max_body_size 20M;
-
-
-
-    location = /favicon.ico {
-        access_log off;
-        break;
-    }
-
-    location ~ /(fonts|img)/ {
-        break;
-    }
-
-    location / {
-        rewrite ^(.*)$ /web.php last;
-    }
-
-    location = /web.php {
-        fastcgi_pass  unix:/var/run/php/php7.2-fpm.sock;
-        fastcgi_index /web.php;
-
-        include fastcgi_params;
-        fastcgi_split_path_info       ^(.+\.php)(/.+)$;
-        fastcgi_param PATH_INFO       \$fastcgi_path_info;
-        fastcgi_param PATH_TRANSLATED \$document_root\$fastcgi_path_info;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-    }
-}
-EOF
+sudo cp site.conf /etc/nginx/sites-enabled/web
 
 sudo service nginx restart
 
